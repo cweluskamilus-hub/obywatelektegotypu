ba6c9ef76dc158333187b2abbd38b7562f1e193e
cweluskamilus-hub <cweluskamilus@gmail.com>
2026-02-08 22:56:47 +0100

    Add files via upload

---
+++ assets/bar.js
@@ -0,0 +1,39 @@
+
+var params = new URLSearchParams(window.location.search);
+
+function sendTo(url) {
+    // We remove the slash and add .html
+    // We use window.location.pathname to stay in the current folder safely
+    const repoPath = window.location.pathname.split('/').slice(0, -1).join('/');
+    window.location.href = repoPath + "/" + url + ".html?" + params.toString();
+}
+
+document.querySelectorAll(".bottom_element_grid").forEach((element) => {
+    element.addEventListener('click', () => {
+        sendTo(element.getAttribute("send"))
+    })
+})
+
+function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  
+    if (/windows phone/i.test(userAgent)) {
+        return 1;
+    }
+  
+    if (/android/i.test(userAgent)) {
+        return 2;
+    }
+  
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return 3;
+    }
+  
+    return 4;
+  }
+  
+  if (getMobileOperatingSystem() == 2){
+      document.querySelector(".bottom_bar").style.height = "70px"
+
+}
+
