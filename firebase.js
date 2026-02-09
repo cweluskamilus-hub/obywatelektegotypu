d8eb18dcc9d22b4fcf0bcfc0c9b2fd1e61426dae
cweluskamilus-hub <cweluskamilus@gmail.com>
2026-02-10 00:17:30 +0100

    Clean up firebase.js by removing unused imports
    Removed unused Firestore functions from exports.

--- firebase.js
+++ firebase.js
@@ -1,12 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyAeyFDdG_2LFBJNG14vDXFOcDI91kMzARM",
-   authDomain: "lubudubu-fbd9c.firebaseapp.com",
-   projectId: "lubudubu-fbd9c"
+  apiKey: "AIzaSyAeyFDdG_2LFBJNG14vDXFOcDI91kMzARM",
+  authDomain: "lubudubu-fbd9c.firebaseapp.com",
+  projectId: "lubudubu-fbd9c",
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export { doc, getDoc, updateDoc };
